44f2c5082afcea2fd613f935d118c1f583f47df8
Tarun Bhardwaj <tarun.bhardwaj@openlabs.co.in>
2014-12-05 00:18:06 +0530

    Added tryton_shell

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='tryton_shell',
+    version='1.0',
+    description='Interactive Shell for Tryton',
+    author='Tarun Bhardwaj, Vishesh Yadav',
+    author_email='tarun.bhardwaj@openlabs.co.in',
+    url='https://github.com/tarunbhardwaj/tryton_shell',
+    scripts=['tryton_shell'],
+)
